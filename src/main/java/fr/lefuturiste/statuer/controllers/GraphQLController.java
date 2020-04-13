26ece51001eebef764be59d1aad0a33430dcb64c
Matthieu Bessat <spamfree@matthieubessat.fr>
2020-04-13 23:47:19 +0200

    fix(GraphQL): return error when invalid json body

--- src/main/java/fr/lefuturiste/statuer/controllers/GraphQLController.java
+++ src/main/java/fr/lefuturiste/statuer/controllers/GraphQLController.java
@@ -7,19 +7,23 @@ import graphql.ExecutionInput;
 import graphql.ExecutionResult;
 import graphql.GraphQL;
 import graphql.schema.GraphQLSchema;
+import org.json.JSONException;
 import org.json.JSONObject;
 import spark.Route;
-
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.Map;
 
 public class GraphQLController {
     public static Route execute = (req, res) -> {
         GraphQLSchema graphQLSchema = new GraphQLJpaSchemaBuilder(HibernateService.getEntityManager()).build();
         GraphQL build = GraphQL.newGraphQL(graphQLSchema).build();
-        JSONObject jsonObject = new JSONObject(req.body());
+        JSONObject jsonObject;
+        try {
+            jsonObject = new JSONObject(req.body());
+        } catch (JSONException exception) {
+            res.status(400);
+            return new JSONObject()
+                    .put("error", "Invalid JSON input")
+                    .put("success", false);
+        }
         ExecutionInput executionInput = ExecutionInput.newExecutionInput()
                 .query(jsonObject.getString("query"))
                 .build();
